bc92f8d16351df342638dfcafe8e881d231cb2a0
Maksym <90103151+MaksVibe@users.noreply.github.com>
2023-05-11 20:02:17 +0300

    Add Breadcrumbs component, and it's styles

---
+++ src/components/Breadcrumbs/Breadcrumbs.styles.ts
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  margin-bottom: 16px;
+`;
+export const Text = styled.p`
+  text-transform: capitalize;
+  color: blue;
+`;
+
+export const Star = styled.div`
+  color: #f7d060;
+  font-size: 1.4em;
+  margin: 0 5px;
+`;
+
+export const StarsWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  color: black;
+`;
---
+++ src/components/Breadcrumbs/Breadcrumbs.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import * as Styled from "./Breadcrumbs.styles";
+import { FaStar, FaAngleRight } from "react-icons/fa";
+
+export interface BreadcrumbsProps {
+  repo: {
+    name: string;
+    owner: string;
+    stars: number;
+  };
+}
+
+export const Breadcrumbs: React.FC<BreadcrumbsProps> = ({ repo }) => {
+  return (
+    <Styled.Wrapper>
+      <Styled.Text>{repo.owner}</Styled.Text>
+      <FaAngleRight color="blue" />
+      <Styled.Text>{repo.name}</Styled.Text>
+      <Styled.StarsWrapper>
+        <Styled.Star>
+          <FaStar />
+        </Styled.Star>
+        {repo.stars > 999999
+          ? `${repo.stars.toString().slice(0, 6)} M `
+          : repo.stars > 999
+          ? `${repo.stars.toString().slice(0, 3)} K `
+          : `${repo.stars} `}
+        stars
+      </Styled.StarsWrapper>
+    </Styled.Wrapper>
+  );
+};
